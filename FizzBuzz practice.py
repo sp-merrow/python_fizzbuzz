e996cb0b61bcd577f7d38c882eb6a617e68c8edc
sp-merrow <87612795+sp-merrow@users.noreply.github.com>
2021-07-22 03:42:02 -0400

    added file

---
+++ FizzBuzz practice.py
@@ -0,0 +1,11 @@
+fizzBuzz = {3 : 'Fizz', 5 : 'Buzz', 8 : 'Boof'}
+
+for i in range(1, 101):
+    charStr = ''
+    for f in fizzBuzz.keys():
+        if i % f == 0:
+            charStr += fizzBuzz[f]
+    if not charStr:
+        print(i)
+    else:
+        print(charStr)
